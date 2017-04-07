c4154614439f1a4f99602ff14864ca18d519b006
cSwormstedt <cswormstedt7@gmail.com>
2017-04-07 10:36:29 -0500

    db update

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -6,15 +6,31 @@ class ApplicationController < Sinatra::Base
 	# require 'twilio-ruby'
 
 	# account_sid = 'AC2200e4e159852a7b5f227158058113c0'
-	# auth_token = 'AUTH'
+	# auth_token = '[AUTH]'
 
 	enable :sessions
 
-	ActiveRecord::Base.establish_connection(
-		adapter: 'postgresql',
-		database: 'lobster'
+	
+	db = URI.parse(ENV['DATABASE_URL'])
 
-		)
+  	DB_NAME = db.path[1..-1]
+
+	  ActiveRecord::Base.establish_connection(
+	    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+	    :host     => db.host,
+	    :port     => db.port,
+	    :username => db.user,
+	    :password => db.password,
+	    :database => DB_NAME,
+	    :encoding => 'utf8'
+	  )
+	
+
+	# ActiveRecord::Base.establish_connection(
+	# 	adapter: 'postgresql',
+	# 	database: 'lobster'
+
+	# 	)
 
 	set :views, File.expand_path('../../views', __FILE__)
 	set :public_dir, File.expand_path('../../public', __FILE__)
