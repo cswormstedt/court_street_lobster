3f3a3174d8a46cbfb758555d49271bea3f8fae6d
cSwormstedt <cswormstedt7@gmail.com>
2017-04-07 11:05:41 -0500

    cleaned up some

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,7 +10,6 @@ var objectToSend = {items: []}
     $('.plus').on('click', function(e){
         var item_id = $(this).prev().prev()[0].value
         var amount = $(this).prev().val()
-        console.log(this)
         amount++
         $(this).prev().attr("value", amount)
 
@@ -21,23 +20,18 @@ var objectToSend = {items: []}
 //min item amount
     $('.min').on('click', function(e){
         var item_id = $(this).parent()[0].value
-        console.log(this)
         var amount = $(this).next().next().val()
         var button = $(e.target);
         var id = button.data('id')
-        console.log('click' + id)
         if (amount > 0){
         amount--;
         $(this).next().next().attr("value", amount)
-        console.log($(this).next())
         }
     })
 
 //add to cart button which sets everything into motion
 $('.cart').on('click', function(){
-    console.log('btn working')
     getEverything(makeAjax)
-
 })
 //////////
 ///ajax///
