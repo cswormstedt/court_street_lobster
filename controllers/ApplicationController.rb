b3c4a50bf8752de77db46b683ea9e3f20c1ae1b0
cSwormstedt <cswormstedt7@gmail.com>
2017-04-11 15:45:32 -0500

    heroku add

--- controllers/ApplicationController.rb
+++ controllers/ApplicationController.rb
@@ -10,27 +10,27 @@ class ApplicationController < Sinatra::Base
 
 	enable :sessions
 
+# heroku
+	db = URI.parse(ENV['DATABASE_URL'])
+
+  	DB_NAME = db.path[1..-1]
+
+	  ActiveRecord::Base.establish_connection(
+	    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+	    :host     => db.host,
+	    :port     => db.port,
+	    :username => db.user,
+	    :password => db.password,
+	    :database => DB_NAME,
+	    :encoding => 'utf8'
+	  )
 	
-	# db = URI.parse(ENV['DATABASE_URL'])
+	# local
+	# ActiveRecord::Base.establish_connection(
+	# 	adapter: 'postgresql',
+	# 	database: 'lobster'
 
- #  	DB_NAME = db.path[1..-1]
-
-	#   ActiveRecord::Base.establish_connection(
-	#     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-	#     :host     => db.host,
-	#     :port     => db.port,
-	#     :username => db.user,
-	#     :password => db.password,
-	#     :database => DB_NAME,
-	#     :encoding => 'utf8'
-	#   )
-	
-
-	ActiveRecord::Base.establish_connection(
-		adapter: 'postgresql',
-		database: 'lobster'
-
-		)
+	# 	)
 
 	set :views, File.expand_path('../../views', __FILE__)
 	set :public_dir, File.expand_path('../../public', __FILE__)
